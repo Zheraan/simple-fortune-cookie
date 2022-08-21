aa1e6271717810f23d9262259869af6cb648740d
Ábel Kováts <19kabel98@gmail.com>
2022-08-21 22:43:41 +0000

    Fixes

--- app_start.sh
+++ app_start.sh
@@ -2,19 +2,19 @@
 if [ $1=="main" ]
 then
     echo "Running $1 deployment."
-    BRANCH=main docker-compose up
+    #BRANCH=main docker-compose up
 elif [ $1=="staging" ]
 then
     echo "Running $1 deployment."
-    BRANCH=staging docker-compose up
+    #BRANCH=staging docker-compose up
 elif [ $1=="production" ]
 then
     echo "Running $1 deployment."
-    BRANCH=production docker-compose up
+    #BRANCH=production docker-compose up
 elif [ $1=="development" ]
 then
     echo "Running $1 deployment."
-    BRANCH=development docker-compose up
+    #BRANCH=development docker-compose up
 else
     echo "Something went wrong."
 fi
